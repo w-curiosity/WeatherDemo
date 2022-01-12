83b576fb2c862f49882475b557b53c97eee003b1
w-curiosity <wu380652029@126.com>
2022-01-11 20:22:02 -0500

    fetched data

--- WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/Views/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     
-    // initatize locationManager
+    // initatize locationManager。
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     
     
     var body: some View {
         VStack {
             
             if let location = locationManager.location {
-                Text("Your coordinates are:\(location.longitude),\(location.latitude)")
+                // Text("Your coordinates are:\(location.longitude),\(location.latitude)")
+                if let weather = weather {
+                    Text("Weather data fetched!")
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
              } else {
                  if locationManager.isLoading {
                      LoadingView()
